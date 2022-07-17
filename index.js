cb8e0d4637c9ec68e7393ac9d7924493ca003cfe
shravanatirtha <shravz.ceo@gmail.com>
2022-07-17 20:46:10 +0530

    bug in file download

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const multer = require("multer");
 const zlib = require("zlib");
@@ -7,23 +8,28 @@ let path = require("path");
 var app = express();
 app.use(cors());
 var port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 // -------------------------------------------- //
 var storage = multer.memoryStorage();
 var upload = multer({
+    dest: "./upload",
   storage: storage,
 });
 // -------------------------------------------- //
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
-app.post("/compress", upload.single("files"), async (req, res) => {
+
+app.post("/compress", upload.single("file"), async (req, res) => {
   try {
-    const destination = `compressed/${req.file.originalname}.gz`;
+    const destination = `upload/${req.file.originalname}.gz`;
     let fileBuffer = req.file.buffer;
     await zlib.gzip(fileBuffer, (err, response) => {
-      if (err) console.log(err);
+      if (err) {
+        console.log(err);
+      }
       fs.writeFile(path.join(__dirname, destination), response, (err, data) => {
-        if (err) console.log(err);
+        if (err) {
+          console.log(err);
+        }
         res.download(path.join(__dirname, destination));
       });
     });
@@ -31,4 +37,7 @@ app.post("/compress", upload.single("files"), async (req, res) => {
     console.log(err);
     res.json(err);
   }
+});
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
